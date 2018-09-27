1aaa74a3151c081b659833c4140db2ec28171b03
panhao <287339387@qq.com>
2018-09-27 11:51:32 +0800

    修改mock配置

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,14 +1,12 @@
-import assign from 'core-js/library/fn/object/assign'
 import {getList,addEvent,removeEvent,updateEvent,batchDelete} from './mock/event2';
 import gridItems from './mock/gridItems';
 
-
-const gram1 = {
+export default {
+  /* 入格事项相关接口 */
+  ...gridItems,
   'POST /api/events': (req, res) => getList(req, res),
   'POST /api/addevents': (req, res) => addEvent(req, res),
   'POST /api/remove': (req, res) => removeEvent(req, res),
   'POST /api/update': (req, res) => updateEvent(req, res),
   'POST /api/batchDelete': (req, res) => batchDelete(req, res),
 };
-
-export default assign(gram1, gridItems);
